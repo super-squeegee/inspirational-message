9a07f972f82e979b41c1547b2c330e07dfa53653
super_squeegee <48956259+super-squeegee@users.noreply.github.com>
2019-04-03 10:16:08 -0400

    Add files via upload

---
+++ message.java
@@ -0,0 +1,13 @@
+import java.io.*;				//lets us prepare for an IOException from getting input from the keyboard
+   import java.util.*;
+
+   public class message
+   {
+      public static void main(String[] args){
+         int g=1;
+         while(g==1)
+         {
+            System.out.println("sarah is cute and lovable and smart and i love her lots <3");
+         }
+   }
+}
